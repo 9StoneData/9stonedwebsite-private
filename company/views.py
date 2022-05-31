2e17608c459b9d369b7a12535a114225c9daacd1
Sean Robert Sprague Sr <seanrobertspraguesr@gmail.com>
2022-05-31 17:05:43 -0400

    first production launch working

--- company/views.py
+++ company/views.py
@@ -6,9 +6,10 @@ import pandas as pd
 def companies(request):
     qs=Company.objects.filter(State="AZ")
     df=pd.DataFrame.from_records(qs.values())
-
+    print(df.shape)
     df['Center_Coord']=list(zip(df.lat,df.lon))
     context = {
         "object": df['Center_Coord']
     }
+    print("DF ready to pass")
     return render(request, "company/companies.html", context=context)
