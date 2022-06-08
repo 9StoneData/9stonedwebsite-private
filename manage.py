b7d9674b13b1feedc17856996d68d2c53eda8ea6
Sean Robert Sprague Sr <seanrobertspraguesr@gmail.com>
2022-06-07 23:23:38 -0400

    updating manage,py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from decouple import config
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
+ 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',  f'{config("PROJECT_NAME")}.settings')
     try:
         from django.core.management import execute_from_command_line
